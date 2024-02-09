0f77c32f1e32b427ea50c2455be8b068ffad910b
Shane Denney Cuizon <shanedenney.cuizon@cit.edu>
2024-02-09 13:25:17 +0800

    create personal information and family profile tables

---
+++ StudentDataDB/Tables/Family_Profile.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Family_Profile]
+(
+	[StudentId] INT NULL,
+	CONSTRAINT [FK_IDStudent] FOREIGN KEY ([StudentId]) REFERENCES [dbo].[Personal_Info] ([IdNumber]), 
+    
+)
---
+++ StudentDataDB/Tables/Personal_Info.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Personal_Info]
+(
+	[IdNumber] INT NOT NULL PRIMARY KEY, 
+    [LastName] NCHAR(40) NOT NULL, 
+    [FirstName] NCHAR(40) NOT NULL, 
+    [MiddleName] NCHAR(40) NULL, 
+    [Address] NCHAR(40) NOT NULL, 
+    [FundSource] NCHAR(40) NOT NULL, 
+    [FundSourceType] NCHAR(40) NOT NULL, 
+    [Job] NCHAR(40) NOT NULL, 
+    [SalaryEnough] NCHAR(40) NOT NULL, 
+    [SalaryRange] NCHAR(40) NOT NULL
+)
