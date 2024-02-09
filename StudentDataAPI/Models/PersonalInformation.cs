0dce89777ed476323e45500c17d76f3646257431
Shane Denney Cuizon <shanedenney.cuizon@cit.edu>
2024-02-09 14:48:51 +0800

    Create personal information model

---
+++ StudentDataAPI/Models/PersonalInformation.cs
@@ -0,0 +1,17 @@
+﻿namespace StudentDataAPI.Models
+{
+    public class PersonalInformation
+    {
+        public int IdNumber { get; set; }
+        public string Lastname { get; set; }
+        public string Firstname { get; set; }
+        public string? Middlename { get; set; }
+        public string Address { get; set;}
+
+        public string FundSource { get; set; }
+        public string FundSourceType { get; set; }
+        public string Job { get; set; }
+        public string SalaryEnough { get; set; }
+        public string SalaryRange { get; set; }
+    }
+}
