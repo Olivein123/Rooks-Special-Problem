1cb02910cd2fdbb1b814b8fd899450531bcffa4b
Shane Denney Cuizon <shanedenney.cuizon@cit.edu>
2024-02-09 14:49:07 +0800

    Create family profile model

---
+++ StudentDataAPI/Models/FamilyProfile.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentDataAPI.Models
+{
+    public class FamilyProfile
+    {
+        public int StudentId { get; set; }
+        public string MotherOccupation { get; set; }
+        public string FatherOccupation { get; set; }
+        public int TotalIncome { get; set;}
+        public int TotalSiblings { get; set; }
+        public int ParentDependentSiblings { get; set; }
+        public int GraduatedSiblings { get; set; }
+    }
+}
