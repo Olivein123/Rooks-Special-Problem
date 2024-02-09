0755f726ed914b695983e977a053af71fc8eddcb
[Erin Rosales Asia] <[erin.asia@cit.edu]>
2024-02-09 15:40:06 +0800

    Add SHSInformation Model and SHS_Information table

---
+++ StudentDataAPI/Models/SHSInformation.cs
@@ -0,0 +1,18 @@
+﻿namespace StudentDataAPI.Models
+{
+    public class SHSInformation
+    {
+        public int StudentId { get; set; }
+        public string ? TypeOfSchool { get; set;}
+        public string ? SeniorHighSchool { get; set; }
+        public string ? Strand { get; set; }
+        public string ? WithHonors { get; set; }
+        public string ? TopNotcher { get; set; }
+        public string ? AcademicRank { get; set; }
+        public string ? AcademicExcellenceAward { get; set; }
+        public string ? GradeRangeEnglish { get; set; }
+        public string ? GradeRangeMath { get; set; }
+        public string ? GradeRangeScience { get; set; }
+        public double GPA { get; set; }
+    }
+}
---
+++ StudentDataDB/Tables/SHS_Information.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[SHS_Information]
+(
+	[StudentId] INT NOT NULL, 
+    [TypeOfSchool] NCHAR(40) NULL, 
+    [SeniorHighSchool] NCHAR(40) NULL, 
+    [Strand] NCHAR(40) NULL, 
+    [WithHonors] NCHAR(40) NULL, 
+    [TopNotcher] NCHAR(40) NULL, 
+    [AcademicRank] NCHAR(40) NULL, 
+    [AcademicExcellenceAward] NCHAR(40) NULL, 
+    [GradeRangeEnglish] NCHAR(40) NULL, 
+    [GradeRangeMath] NCHAR(40) NULL,
+    [GradeRangeScience] NCHAR(40) NULL,
+    [GPA] NCHAR(40) NULL,
+    CONSTRAINT [FK_StudentId] FOREIGN KEY ([StudentId]) REFERENCES [dbo].[Personal_info] ([IdNumber])
+)
+
