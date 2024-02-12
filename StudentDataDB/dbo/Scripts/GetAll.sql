b8f5866ddc2c667ee61e6f9d4f176d3e8a17c6b3
Shane Denney Cuizon <shanedenney.cuizon@cit.edu>
2024-02-12 22:50:09 +0800

    create scripts for clearing the tables and viewing all data

--- StudentDataDB/dbo/Scripts/GetAll.sql
+++ StudentDataDB/dbo/Scripts/GetAll.sql
@@ -1,1 +1,4 @@
-﻿SELECT * FROM Personal_Info
+﻿SELECT * FROM Personal_Info P
+INNER JOIN Family_Profile F on P.IdNumber=F.StudentId
+INNER JOIN SHS_Information S on P.IdNumber = S.StudentId
+INNER JOIN College_Information C on P.IdNumber = C.StudentId
