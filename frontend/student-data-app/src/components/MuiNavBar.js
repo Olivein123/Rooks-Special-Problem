77623fb75729af01620d1c14c1b7fe82fd750665
Shane Denney Cuizon <shanedenney.cuizon@cit.edu>
2024-03-07 20:01:00 +0800

    Create NavBar with side drawer

---
+++ frontend/student-data-app/src/components/MuiNavBar.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import { Link } from 'react-router-dom';
+const MuiNavBar = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        {[
+          {text: 'Home', route:'/'},
+          {text: 'Personal Profile', route:'/personal-info'},
+          {text: 'Family Background', route: '/family-bg'},
+          {text: 'SHS Information', route: '/senior-info'},
+          {text: 'College Information', route: '/college-info'}
+        ].map((item, index) => (
+          <ListItem key={item.text} disablePadding>
+            <ListItemButton component={Link} to={item.route}>
+              {/* <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon> */}
+              <ListItemText primary={item.text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+    </Box>
+  );
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }} onClick={toggleDrawer(true)}>
+          <MenuIcon />
+        </IconButton>
+        <Drawer open={open} onClose={toggleDrawer(false)}>
+        {DrawerList}
+      </Drawer>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Student Data 
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  </Box>
+  );
+}
+
+export default MuiNavBar
