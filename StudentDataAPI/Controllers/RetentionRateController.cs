4c9732665ed7e5b5ae5363b5f20dade56998c956
Olivein Kurl Potolin <oliveinkurl.potolin@cit.edu>
2024-04-29 16:53:40 +0800

    Add and tune ML mode in app

---
+++ StudentDataAPI/Controllers/RetentionRateController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.ML;
+using static StudentDataAPI.RetentionRateModel;
+
+namespace StudentDataAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RetentionRateController : ControllerBase
+    {
+        private static MLContext mlContext = new MLContext();
+        private static ITransformer model = mlContext.Model.Load("RetentionRateModel.zip", out var modelInputSchema);
+
+
+        [HttpPost]
+        public ActionResult<ModelOutput> Predict([FromBody] ModelInput input)
+        {
+            var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(model);
+            var result = predEngine.Predict(input);
+            return Ok(result);
+        }
+    }
+}
